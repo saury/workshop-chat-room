5c610c0007e0ea6e7ec28a08695fb93ce851eaef
Jin Xu <jin.xu@ef.com>
2018-03-30 16:14:15 +0800

    auth the username and pwd when call msgs apis

--- src/modules/authentication/setup.ts
+++ src/modules/authentication/setup.ts
@@ -2,18 +2,26 @@ import { Express } from 'express';
 import * as passport from 'passport';
 import { BasicStrategy } from 'passport-http';
 
+import { db, tables } from 'modules/db';
+
 import { authenticate } from './authenticate';
 
 export const setup = (server: Express) => {
     server.use(passport.initialize());
 
     passport.use(
-        new BasicStrategy((username, password, done) => {
-            if (username === 'jin' && password === 'jin') {
-                return done(null, {});
-            } else {
+        new BasicStrategy(async (username, password, done) => {
+            const { Items: users } = await db.doc.scan({ TableName: tables.users }).promise();
+
+            const findUser = users!.find((_user) => {
+                return _user.username === username;
+            });
+
+            if (!findUser || password !== findUser.password) {
                 return done(null, false);
             }
+
+            return done(null, {});
         }),
     );
 
