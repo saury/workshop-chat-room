4a121364346cc6c7827e436308af2dc8291240e4
Jin Xu <jin.xu@ef.com>
2018-03-30 11:01:42 +0800

    add post controller

--- src/modules/db/messages/mount.ts
+++ src/modules/db/messages/mount.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
 
 import { controller as getController } from './get';
+import { controller as postController } from './post';
 import { route } from './route';
 
 export const mount = (app: Express) => {
     app.get(route, getController);
+    app.post(route, postController);
 };
