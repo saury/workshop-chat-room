3624d012b38f6a810a8eb350d3954fe8d2869ea5
Jin Xu <jin.xu@ef.com>
2018-03-30 10:58:09 +0800

    move msg ctrller into its get folder to get a better structure

--- src/modules/db/messages/mount.ts
+++ src/modules/db/messages/mount.ts
@@ -1,8 +1,8 @@
 import { Express } from 'express';
 
-import { controller } from './controller';
+import { controller as getController } from './get';
 import { route } from './route';
 
 export const mount = (app: Express) => {
-    app.get(route, controller);
+    app.get(route, getController);
 };
